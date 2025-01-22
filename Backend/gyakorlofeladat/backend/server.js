a81b1056706a0dd757ff06f544685b7982ee6087
TaicsMrt <taimar487@hengersor.hu>
2025-01-22 10:24:26 +0100

    25.01.22

---
+++ Backend/gyakorlofeladat/backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express ();
+const  cors = require("cors");
+const mysql = require('body-parser');
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    port: 3307,
+    password: "",
+    database: "kozutak"
+});
+app.get("/", (req, res) =>(
+    res.send("Express is running")
+))
