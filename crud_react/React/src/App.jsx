86c0df8d0a07f2eff259883094d2dd2b595ef87f
TaicsMrt <taimar487@hengersor.hu>
2025-01-31 13:21:05 +0100

    01.31

--- crud_react/React/src/App.jsx
+++ crud_react/React/src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import{BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from 'Home'
+import Regiok from 'Regiok'
 function App() {
-
-
-return(
-  
-  <div>
-    
-  </div>
-
-)
+  return(
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="Regiok" element={<Regiok />}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
 
 
 
---
+++ crud_react/React/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return(
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1></h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
